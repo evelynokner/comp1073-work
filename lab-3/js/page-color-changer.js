1effd4da97b908a04507fa25e0dd7974c6f1d595
lyneok <67384872+lyneok@users.noreply.github.com>
2025-06-11 18:27:24 -0400

    declared & initialized constants for html elements

---
+++ lab-3/js/page-color-changer.js
@@ -0,0 +1,5 @@
+// declare and initialize constants
+const pageBody = document.querySelector('html');
+const redSlider = document.querySelector('#red');
+const greenSlider = document.querySelector('#green');
+const blueSlider = document.querySelector('#blue');
