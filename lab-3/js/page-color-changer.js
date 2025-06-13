99f102dca8bb0aaf5b1a8c56f0ca65faac8a795b
lyneok <67384872+lyneok@users.noreply.github.com>
2025-06-13 15:32:40 -0400

    function to change page bg color based on slider

--- lab-3/js/page-color-changer.js
+++ lab-3/js/page-color-changer.js
@@ -3,3 +3,17 @@ const pageBody = document.querySelector('html');
 const redSlider = document.querySelector('#red');
 const greenSlider = document.querySelector('#green');
 const blueSlider = document.querySelector('#blue');
+
+// function to change page background color
+function changeBackgroundColor(){
+    // vars to capture the current value of each slider when it is moved
+    let redValue = redSlider.value;
+    let greenValue = greenSlider.value;
+    let blueValue = blueSlider.value;
+
+    // change background color based on current slider values
+    pageBody.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+
+    // testing to check slider values
+    console.log(`${redValue}, ${greenValue}, ${blueValue}`);
+}
