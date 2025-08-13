2872053676615278b997596dfb07687ce0e8211b
lyneok <67384872+lyneok@users.noreply.github.com>
2025-08-13 12:53:47 -0400

    made smoothie object class

---
+++ assignment-2/js/smoothie.js
@@ -0,0 +1,14 @@
+/*
+Create a class for a smoothie object that follows best practices as demonstrated in class.
+Visually output a description of the customer’s smoothie to the HTML page.
+*/
+
+// smoothie object & constructor
+class Smoothie {
+    constructor(size, liquid, fruits, berries){
+        this.size = size;
+        this.liquid = liquid;
+        this.fruits = fruits;
+        this.berries = berries;
+    }
+}
